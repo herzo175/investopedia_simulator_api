cb802473ce0743e73b39155d066957420de16d29
Jeremy Herzog <herzo175@umn.edu>
2018-03-13 00:33:55 -0500

    remove get method and lift closure from route

--- server.py
+++ server.py
@@ -4,83 +4,78 @@ from InvestopediaApi import ita
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def api():
-    if request.method == 'GET':
-        try:
-            actions = {
-                'get_quote': lambda: ita.get_quote(
-                    request.args.get('symbol'))
-            }
+def get_action(req_body):
+    # login if email and password are provided
+    email, password = (
+        req_body.get('email', None), req_body.get('password', None))
+    print(email, password)
 
-            return json.dumps(
-                {'result': actions[request.args.get('function')]()})
-        except Exception as exc:
-            return json.dumps({'error': str(exc)})
-    elif request.method == 'POST':
+    if email is not None and password is not None:
+        client = ita.Account(email, password)
+
+
+    # set default values if not specified and enums 
+    if (
+        req_body['function'] == 'trade' or
+            req_body['function'] == 'trade_option'):
+        req_body['orderType'] = {
+            'buy': ita.Action.buy,
+            'sell': ita.Action.sell,
+            'short': ita.Action.short,
+            'cover': ita.Action.cover
+        }[req_body['orderType']]
+
+        req_body['priceType'] = req_body.get('priceType', 'Market')
+
+        try:
+            req_body['price'] = float(req_body['price'])
+        except KeyError:
+            req_body['price'] = False
+
+        req_body['duration'] = {
+            'day_order': ita.Duration.day_order,
+            'good_cancel': ita.Duration.good_cancel
+        }[req_body.get('duration', 'good_cancel')]
+
+    # return closure for function being called
+    return {
+        'get_quote': lambda: ita.get_quote(req_body['symbol']),
+        'get_portfolio_status': lambda: client.get_portfolio_status(),
+        'get_current_securities': lambda: client.get_current_securities(),
+        'get_open_trades': lambda: client.get_current_trades(),
+        'trade': lambda: (
+            client.trade(
+                req_body['symbol'],
+                req_body['orderType'],
+                int(req_body['quantity']),
+                req_body['priceType'],
+                req_body['price'],
+                req_body['duration']
+            )
+        ),
+        'trade_option': lambda: (
+            client.trade_option(
+                req_body['symbol'],
+                req_body['optionType'],
+                float(req_body['strikePrice']),
+                int(req_body['expire_date']),
+                int(req_body['quantity']),
+                req_body['priceType'],
+                req_body['price'],
+                req_body['duration']
+            )
+        )
+    }[req_body['function']]
+
+
+@app.route('/', methods=['POST'])
+def api():
+    if request.method == 'POST':
         try:
             d = json.loads(request.data)
-            email, password, func = d['email'], d['password'], d['function']
+            print(d)
 
-            if d['function'] == 'trade' or d['function'] == 'trade_option':
-                order_type = {
-                    'buy': ita.Action.buy,
-                    'sell': ita.Action.sell,
-                    'short': ita.Action.short,
-                    'cover': ita.Action.cover
-                }[d['orderType']]
-
-                try:
-                    price_type = d['priceType'],
-                except KeyError:
-                    price_type = 'Market'
-
-                try:
-                    price = float(d['price'])
-                except KeyError:
-                    price = False
-
-                try:
-                    duration = {
-                        'day_order': ita.Duration.day_order,
-                        'good_cancel': ita.Duration.good_cancel
-                    }[d['duration']]
-                except KeyError:
-                    duration = ita.Duration.good_cancel
-
-            def trade():
-                return client.trade(
-                    d['symbol'],
-                    order_type,
-                    int(d['quantity']),
-                    price_type,
-                    price,
-                    duration)
-
-            def trade_option():
-                return client.trade_option(
-                    d['symbol'],
-                    order_type,
-                    d['optionType'],
-                    float(d['strikePrice']),
-                    int(d['expire_date']),
-                    int(d['quantity']),
-                    price_type,
-                    price,
-                    duration
-                )
-
-            client = ita.Account(email, password)
-            actions = {
-                'get_portfolio_status': lambda: client.get_portfolio_status(),
-                'get_current_securities': (
-                    lambda: client.get_current_securities()),
-                'get_open_trades': lambda: client.get_current_trades(),
-                'trade': trade,
-                'trade_option': trade_option
-            }
-
-            return json.dumps({'result': actions[func]()})
+            return json.dumps({'result': get_action(d)()})
         except Exception as exc:
             return json.dumps({'error': str(exc)})
     else:
